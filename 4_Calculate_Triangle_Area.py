05840caaf74884a63c523cc2fc1f0a23a63e597e
Jahmeel Robinson <103872276+jahmeeel@users.noreply.github.com>
2023-02-19 12:21:42 -0500

    Update 4_Calculate_Triangle_Area.py
    Added spacing for more readability.

--- 4_Calculate_Triangle_Area.py
+++ 4_Calculate_Triangle_Area.py
@@ -32,6 +32,9 @@ Review:
 '''
 
 base = float(input("Enter the base of the triangle: "))
+
 height = float(input("Enter the height of the triangle: "))
+
 area = (base * height) / 2
+
 print(f"The area of the triangle is {round(area, 2)}")
